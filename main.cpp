7bb38f4997327b6f4f9a0ac791dafbd2d2c2cda4
Kietzell <enisbilgin@gmail.com>
2019-03-08 15:39:59 +0000

    filelock?

--- main.cpp
+++ main.cpp
@@ -3,8 +3,20 @@
 #include <string.h>
 #include <unistd.h>
 
+
+// timer
+#include <time.h>
+
+
+
+
+
+
 int main (int argc, char* argv[])
 {
+    // some timing
+    int msecond = 0, trigger = 10;
+
     char* file = "/home/ubuntu/MultiRunTest/multiLockTest.txt";
     int fd;
     struct flock lock;
@@ -17,19 +29,32 @@ int main (int argc, char* argv[])
     memset (&lock, 0, sizeof(lock));
     lock.l_type = F_WRLCK;
 /* Place a write lock on the file. */
-    while(fcntl (fd, F_SETLKW, &lock)==-1){
-        printf ("Already Locked! ");
-        sleep(5);
-    };
+    if(fcntl (fd, F_SETLKW, &lock)==-1){
+        printf("Lock not succcessful?");
+    }
+
 
     printf ("locked; hit Enter to unlock... ");
 /* Wait for the user to hit Enter. */
     getchar ();
 
+
     printf ("unlocking\n");
+
+
+
+
 /* Release the lock. */
+    clock_t before = clock();
+
     lock.l_type = F_UNLCK;
     fcntl (fd, F_SETLKW, &lock);
+
+
+    clock_t difference = clock() - before;
+    msecond = difference * 1000 / CLOCKS_PER_SEC;
+
+    printf ("Time Taken to Unlock in Miliseconds: (%d)\n", msecond%1000);
 
     close (fd);
     return 0;
